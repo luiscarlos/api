94ec0ba64fd32e8563843b98af3ad461df7185c8
luiscarlos <luiscarlos8pe@gmail.com>
2024-08-28 21:09:19 -0300

    Criando endpoint para buscar medico por id

--- src/main/java/med/voll/api/resource/MedicoResource.java
+++ src/main/java/med/voll/api/resource/MedicoResource.java
@@ -1,6 +1,5 @@
 package med.voll.api.resource;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -26,53 +25,58 @@ import med.voll.api.repository.MedicoRepository;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
 @RestController
 @RequestMapping("/medicos")
 public class MedicoResource {
 
-	
 	@Autowired
-	private MedicoRepository  medicoRepository;
-	
-	
+	private MedicoRepository medicoRepository;
+
 	@PostMapping
 	@Transactional
 	public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder) {
-	                var medico = new Medico(dados);
-	                medicoRepository.save(medico);
+		var medico = new Medico(dados);
+		medicoRepository.save(medico);
 
-	                var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+		var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
 
-	                return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
+		return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
 	}
-	  
-	 /* @GetMapping
-	  public List<DadosListagemMedico> listar(){
-		  
-		  return medicoRepository.findAll().stream().map(DadosListagemMedico::new).toList();
-	  }*/
-	  
-	  @GetMapping
-	  public ResponseEntity<Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-	        var page = medicoRepository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
-	        return ResponseEntity.ok(page);
-	    }
-	  
-	  @PutMapping
-	  @Transactional
-	  public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
-	  	var medico = medicoRepository.getReferenceById(dados.id());
-	  	medico.atualizarInformacoes(dados);
-	  	return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
-	  }
-	  
-	  @DeleteMapping("/{id}")
-	  @Transactional
-	  public ResponseEntity excluir(@PathVariable Long id ) {
-		  var medico = medicoRepository.getReferenceById(id); 
-		  medico.excluir();
-		  
-		  return ResponseEntity.noContent().build();
-	  }
+
+	/*
+	 * @GetMapping public List<DadosListagemMedico> listar(){
+	 * 
+	 * return
+	 * medicoRepository.findAll().stream().map(DadosListagemMedico::new).toList(); }
+	 */
+
+	@GetMapping
+	public ResponseEntity<Page<DadosListagemMedico>> listar(
+			@PageableDefault(size = 10, sort = { "nome" }) Pageable paginacao) {
+		var page = medicoRepository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+		return ResponseEntity.ok(page);
+	}
+
+	@PutMapping
+	@Transactional
+	public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+		var medico = medicoRepository.getReferenceById(dados.id());
+		medico.atualizarInformacoes(dados);
+		return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+	}
+
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity excluir(@PathVariable Long id) {
+		var medico = medicoRepository.getReferenceById(id);
+		medico.excluir();
+
+		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity buscarPorId(@PathVariable Long id) {
+		var medico = medicoRepository.getReferenceById(id);
+		return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+	}
 }
