9db055be23d2bec33a77eee1829e045a6e815749
luiscarlos <luiscarlos8pe@gmail.com>
2025-02-20 21:08:37 -0300

    Baixando dependencia do auth0

--- src/main/java/med/voll/api/model/Usuario.java
+++ src/main/java/med/voll/api/model/Usuario.java
@@ -1,14 +1,12 @@
 package med.voll.api.model;
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
-
-
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
 
 /*@Getter
@@ -18,19 +16,20 @@ import jakarta.persistence.Table;
 */
 @Table(name = "usuarios")
 @Entity(name = "Usuario")
-public class Usuario {
+public class Usuario implements UserDetails{
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	private String login;
-	private String Senha;
-	
-	
-	
-	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
+	 @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
+	    private String login;
+	    private String senha;
+	
+	
 	public Usuario() {
 		super();
 	}
@@ -40,7 +39,7 @@ public class Usuario {
 		super();
 		this.id = id;
 		this.login = login;
-		Senha = senha;
+		this.senha = senha;
 	}
 
 
@@ -57,10 +56,10 @@ public class Usuario {
 		this.login = login;
 	}
 	public String getSenha() {
-		return Senha;
+		return senha;
 	}
 	public void setSenha(String senha) {
-		Senha = senha;
+		senha = senha;
 	}
 
 
@@ -81,6 +80,47 @@ public class Usuario {
 		Usuario other = (Usuario) obj;
 		return Objects.equals(id, other.id);
 	}
+
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+
+
+   
+
+ 
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 	
 	
 	
---
+++ src/main/java/med/voll/api/resource/AutenticacaoResource.java
@@ -0,0 +1,33 @@
+package med.voll.api.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import med.voll.api.dtos.DadosAutenticacao;
+
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacaoResource {
+  
+	 @Autowired
+	    private AuthenticationManager manager;
+	
+
+	
+	 @PostMapping
+	    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
+	        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+	        var authentication = manager.authenticate(token);
+
+	        return ResponseEntity.ok().build();
+	    }
+	
+}
